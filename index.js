56d2a3a1851078982638e06774623c8507915602
luomaochen <31931646+luomaochen@users.noreply.github.com>
2023-05-14 21:58:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-var axios = require('axios');
-
-var config = {
-   method: 'post',
-   url: 'http://jldl.mkemall.cn/index/homeGoodsList',
-   headers: { 
-      'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)'
-   }
-};
-
-axios(config)
-.then(function (response) {
-   console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-   console.log(error);
-});
+const request = require('request')
+ 
+request.post('http://jldl.mkemall.cn/index/homeGoodsList', {}, (error, res, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(body)
